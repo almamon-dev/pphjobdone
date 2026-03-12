bfb6717935df3c9f9ad7d27acfc54b4ba0a82204
dev-mamon <mamon193p@gmail.com>
2026-03-12 10:42:25 +0600

    update code

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -31,142 +31,44 @@ class UserSeeder extends Seeder
         \App\Models\User::updateOrCreate(
             ['email' => 'user1@gmail.com'],
             [
-                'name' => 'User O`ne',
+                'name' => 'User One',
                 'password' => bcrypt('password'),
                 'profile_setup' => false,
                 'is_verified' => true,
                 'email_verified_at' => now(),
-                'verified_at' => now(),
+                'verified_at' => now(), 
                 'phone' => '0123456788',
                 'gender' => 'female',
             ]
         );
 
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user2@gmail.com'],
-            [
-                'name' => 'User Two',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user3@gmail.com'],
-            [
-                'name' => 'User Three',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user4@gmail.com'],
-            [
-                'name' => 'User Four',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user5@gmail.com'],
-            [
-                'name' => 'User Five',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user6@gmail.com'],
-            [
-                'name' => 'User Six',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user7@gmail.com'],
-            [
-                'name' => 'User Seven',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user8@gmail.com'],
-            [
-                'name' => 'User Eight',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user9@gmail.com'],
-            [
-                'name' => 'User Nine',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
-        // user three
-        \App\Models\User::updateOrCreate(
-            ['email' => 'user10@gmail.com'],
-            [
-                'name' => 'User Ten',
-                'password' => bcrypt('password'),
-                'profile_setup' => true,
-                'is_verified' => true,
-                'email_verified_at' => now(),
-                'verified_at' => now(),
-                'phone' => '0123456787',
-                'gender' => 'male',
-            ]
-        );
+        // user three to ten
+        $usersData = [
+            ['name' => 'User Two', 'email' => 'user2@gmail.com'],
+            ['name' => 'User Three', 'email' => 'user3@gmail.com'],
+            ['name' => 'User Four', 'email' => 'user4@gmail.com'],
+            ['name' => 'User Five', 'email' => 'user5@gmail.com'],
+            ['name' => 'User Six', 'email' => 'user6@gmail.com'],
+            ['name' => 'User Seven', 'email' => 'user7@gmail.com'],
+            ['name' => 'User Eight', 'email' => 'user8@gmail.com'],
+            ['name' => 'User Nine', 'email' => 'user9@gmail.com'],
+            ['name' => 'User Ten', 'email' => 'user10@gmail.com'],
+        ];
+
+        foreach ($usersData as $u) {
+            \App\Models\User::updateOrCreate(
+                ['email' => $u['email']],
+                [
+                    'name' => $u['name'],
+                    'password' => bcrypt('password'),
+                    'profile_setup' => true,
+                    'is_verified' => true,
+                    'email_verified_at' => now(),
+                    'verified_at' => now(), // Added verified_at
+                    'phone' => '0123456787',
+                    'gender' => 'male',
+                ]
+            );
+        }
     }
 }
